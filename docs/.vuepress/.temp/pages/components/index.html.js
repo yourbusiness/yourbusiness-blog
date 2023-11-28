c1a54658d18220fc5e6af4e30be0f0e83ccdd682
marcus <976587939@qq.com>
2023-11-28 14:31:34 +0800

    feat: pnpm

--- docs/.vuepress/.temp/pages/components/index.html.js
+++ docs/.vuepress/.temp/pages/components/index.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-1020f180\",\"path\":\"/components/\",\"title\":\"yourbusiness\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"yourbusiness\",\"author\":\"marcus\",\"date\":\"2023-11-07\"},\"headers\":[{\"level\":2,\"title\":\"默认的页面路由\",\"slug\":\"默认的页面路由\",\"link\":\"#默认的页面路由\",\"children\":[]}],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"components/README.md\"}")
+export const data = JSON.parse("{\"key\":\"v-1020f180\",\"path\":\"/components/\",\"title\":\"yourbusiness\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"yourbusiness\",\"author\":\"marcus\",\"date\":\"2023-11-07\"},\"headers\":[{\"level\":2,\"title\":\"默认的页面路由\",\"slug\":\"默认的页面路由\",\"link\":\"#默认的页面路由\",\"children\":[]}],\"git\":{\"updatedTime\":1701152718000,\"contributors\":[{\"name\":\"marcus\",\"email\":\"976587939@qq.com\",\"commits\":1}]},\"filePathRelative\":\"components/README.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
